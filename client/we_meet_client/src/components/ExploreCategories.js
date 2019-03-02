b20cc6399300d888685076efa514e144e3626d2c
shahin1290 <s.patowary@yahoo.com>
2019-03-02 22:10:03 +0100

    Adds styling to HomePage-ExploreByCategory

--- client/we_meet_client/src/components/ExploreCategories.js
+++ client/we_meet_client/src/components/ExploreCategories.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+const styles = {
+  root: {
+    maxWidth: '1000px',
+    margin: '0 auto',
+    display: 'grid',
+    'grid-template-columns': '1fr 1fr 1fr',
+    'grid-gap': '30px',
+    'align-items': 'stretch'
+  },
+  heading:{
+    maxWidth: '1000px',
+    margin: '50px auto',
+  },
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
 
 class ExploreCategories extends Component {
   constructor(props) {
@@ -23,28 +52,47 @@ class ExploreCategories extends Component {
 
   render() {
     let categories = this.state.categories;
+    const { classes } = this.props;
     let categoriesList = categories.map(category => {
       return (
-        <div key={category.id} inlineBlock style={{marginBottom:"20px"}}>
-          <Link to={{pathname: `/categories/${category.id}`, state: {category}}} style={{ textDecoration: 'none' }}>
-            
-              <div>
-                <img src={category.image || `./assets/images/${category.name.toLowerCase()}.png`} />
-              </div>
-         
-            <div className="category-name">{category.name}</div>
-          </Link>
-        </div>
-      );
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={`./assets/images/${category.name.toLowerCase()}.png`}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {category.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+        
+      )
     });
 
     return (
       <div>
-        <h1>Explore categories</h1>
-        {categoriesList}
+        <h1 className={classes.heading}>Explore by category</h1>
+        <div className={classes.root}>
+          {categoriesList}
+        </div>
       </div>
     );
   }
 }
 
-export default ExploreCategories;
+CardMedia.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ExploreCategories);
