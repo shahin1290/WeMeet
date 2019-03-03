2916fe045bfa03a3571deede80efa82d274d75f6
shahin1290 <s.patowary@yahoo.com>
2019-03-03 19:38:36 +0100

    Adds styling to CategoryView page

--- client/we_meet_client/src/components/CategoryView.js
+++ client/we_meet_client/src/components/CategoryView.js
@@ -1,6 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    maxWidth: '1000px',
+    margin: '0 auto',
+    display: 'grid',
+    'grid-template-columns': '1fr 1fr 1fr',
+    'grid-gap': '30px',
+    'align-items': 'stretch'
+  },
+  heading:{
+    
+    margin: '0 auto 50px auto',
+    height: '200px',
+    'text-align': 'center',
+    'background-image': 'url("../assets/images/hero.jpg")',
+    color: 'white',
+    'font-weight': 'bold',
+    'font-size': '50px'
+  },
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
 
 
  class CategoryView extends Component {
@@ -26,21 +62,48 @@ import { Link } from 'react-router-dom';
 
 
    render() {
-    let groupList = this.state.category.groups.map(group => {
+    let groups = this.state.category.groups;
+    const { classes } = this.props;
+    let groupsList = groups.map(group => {
       return (
-        <Link key={group.id} to={{pathname: `/groups/${group.id}`, state: {group}}} style={{ textDecoration: 'none' }}>
-          <div >{group.name}</div>
-        </Link>        
+        <Card className={classes.card}>
+          <CardActionArea component={Link} to={{pathname: `/groups/${group.id}`, state: {group}}}>
+            <CardMedia
+              className={classes.media}
+              image="../assets/images/frogs.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {group.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+        
       )
-    })
+    });
 
      return (
       <div>
-        {/* <h1>{this.state.category.name}</h1> */}
-        <h2>{groupList}</h2>
+        <div className={classes.heading}>{this.state.category.name}</div>
+        <div className={classes.root}>
+          {groupsList}
+        </div>
       </div>
     );
   }
 }
 
- export default CategoryView; 
+CardMedia.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CategoryView);
