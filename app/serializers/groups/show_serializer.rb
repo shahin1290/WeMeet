84fb354e600602b7a8161db986374d04a67b63be
shahin1290 <s.patowary@yahoo.com>
2019-03-13 22:09:10 +0100

    Updates show serializers for groups members and organizers

--- app/serializers/groups/show_serializer.rb
+++ app/serializers/groups/show_serializer.rb
@@ -5,15 +5,15 @@ class Groups::ShowSerializer < ActiveModel::Serializer
   has_many :future_events, serializer: Events::ForGroupCollectionSerializer
   has_many :past_events, serializer: Events::ForGroupCollectionSerializer
 
-  # def members
-  #   object.members.each.map do |member|
-  #   Users::ShowSerializer.new(member.user)
-  #   end
-  # end
+  def members
+    object.members.each.map do |member|
+    Users::ShowSerializer.new(member.user)
+    end
+  end
 
-  # def organizer
-  #   Users::ShowSerializer.new(object.organizer)
-  # end
+  def organizer
+    Users::ShowSerializer.new(object.organizer)
+  end
 
 end
 
